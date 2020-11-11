4322d0324daa35ccfed4ec54e1f559ac446e4e8e
modhvadiyac25 <59826685+chetanmaher@users.noreply.github.com>
2020-11-11 15:35:55 +0530

    ..

--- QuizHub/QuizHub/Models/Quiz_question.cs
+++ QuizHub/QuizHub/Models/Quiz_question.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace QuizHub.Models
 {
     public class Quiz_question
     {
         public int qq_id { get; set; }
-
-        public int qc_id { get; set; }
 
         public string qq_opta { get; set; }
 
@@ -21,7 +20,7 @@ namespace QuizHub.Models
 
         public string qq_ans { get; set; }
 
-        public int qc_id { get; set; }
+        public Quiz_category qc_id { get; set; }
         public Quiz_category quiz_Category { get; set; }
 
     }
@@ -30,14 +29,6 @@ namespace QuizHub.Models
 
 /*quiz_questions
 
-Qq_id primary key, auto increment
-Qc_id foreigen key (Quiz_category, One To Many)
-Q_question varchar(200)
-Qq_opta varchar(100)
-Qq_optb varchar(100)
-Qq_optc varchar(100)
-Qq_optd varchar(100)
-Qq_ans varchar(100)
 
   [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
  */
