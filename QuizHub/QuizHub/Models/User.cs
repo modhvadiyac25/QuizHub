c8bec8b142ccd608e4cef30b9ab2ef02917bc732
chetanmaher <59826685+chetanmaher@users.noreply.github.com>
2020-11-11 11:15:42 +0530

    model (user)

---
+++ QuizHub/QuizHub/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace QuizHub.Models
+{
+    public class User
+    {
+        [Required]
+        public int u_id { get; set; }
+
+        [Required]
+        public string fname { get; set; }
+
+        [Required]
+        public string lname { get; set; }
+
+        [Required]
+        public string gender { get; set; }
+
+        [Required,DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+
+        [Required]
+        public string mno { get; set; }
+
+        [Required]
+        [MinLength(8,ErrorMessage ="Password must be greater then 8 characters !!")]
+        public string password { get; set; }
+
+    }
+}
---
+++ QuizHub/QuizHub/Models/User_progress.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizHub.Models
+{
+    public class User_progress
+    {
+    }
+}
